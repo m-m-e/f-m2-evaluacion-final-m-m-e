6da9c1ab29880d0a0c36e239a1043aa429485fb9
Megan Edwards <megmyfedwards@gmail.com>
2019-04-22 08:37:19 +0200

    Add javascript structure

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('>> Ready :)');
+//elements to use
+input
+button
+results
+
+//function to get input text
+
+
+//function to fetch data
+
+
+//function to print series WITHIN CONTAINER
+
+
+//function to add checkbox for favourites
+
+
+//function to apply style changes to favourites
+
+
+//function to save favourites 
+
+
+//add listener to button
